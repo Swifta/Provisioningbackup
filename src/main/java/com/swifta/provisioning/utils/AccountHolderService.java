419d16e63260b32df20fdda88dbfedb890d83c90
oonwuzu <oonwuzu@swifta.com>
2014-11-11 09:01:20 +0100

    after implementing link account

--- src/main/java/com/swifta/provisioning/utils/AccountHolderService.java
+++ src/main/java/com/swifta/provisioning/utils/AccountHolderService.java
@@ -8,10 +8,36 @@ import com.swifta.sub.mats.operation.data.MatsdataserviceStub.Activationrequest;
 import com.swifta.sub.mats.operation.data.MatsdataserviceStub.Activationresponse;
 import com.swifta.sub.mats.operation.data.MatsdataserviceStub.Activationresponses;
 import com.swifta.sub.mats.operation.data.MatsdataserviceStub.ActivationresponsesE;
+import com.swifta.sub.mats.operation.data.MatsdataserviceStub.Linkaccountresponse;
+import com.swifta.sub.mats.operation.data.MatsdataserviceStub.Linkaccountresponses;
+import com.swifta.sub.mats.operation.data.MatsdataserviceStub.LinkaccountresponsesE;
+import com.swifta.sub.mats.operation.data.MatsdataserviceStub.Linkccountrequest;
 import com.swifta.sub.mats.operation.provisioning.v1.Credentials;
 
 public class AccountHolderService {
 	MatsdataserviceStub matsStub = null;
+
+	public String linkAccount(String childUsername, String parentUsername,
+			int profileId, String reason) throws RemoteException,
+			DataServiceFault {
+		String statusMessage = "";
+		matsStub = new MatsdataserviceStub();
+		Linkccountrequest linkAccountRequest = new Linkccountrequest();
+		linkAccountRequest.setChilduserresourceid(childUsername);
+		linkAccountRequest.setParentaccountresourceid(parentUsername);
+		linkAccountRequest.setProfileid(profileId);
+		linkAccountRequest.setReason(reason);
+		LinkaccountresponsesE linkAccountResponses = matsStub
+				.linkccountrequest(linkAccountRequest);
+		Linkaccountresponses linkAccountResponse = linkAccountResponses
+				.getLinkaccountresponses();
+		Linkaccountresponse[] linkResponseArray = linkAccountResponse
+				.getLinkaccountresponse();
+		for (Linkaccountresponse singleResponse : linkResponseArray) {
+			statusMessage = singleResponse.getStatusMessage();
+		}
+		return statusMessage;
+	}
 
 	public String activateUser(String username, Credentials credential,
 			String securityAnswer, String identificaitonNo, String bankNo,
@@ -37,4 +63,5 @@ public class AccountHolderService {
 		}
 		return statusMessage;
 	}
+
 }
